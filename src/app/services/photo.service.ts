16914d65f120c49ef377a5c6f71695f50c077388
Luan Teixeira <luanlts@outlook.com>
2023-08-28 16:26:02 -0300

    feat: adiciona foto no sistema de arquivos
    utiliza api do sistema de arquivo do dispositivo para armazenar cada
    foto tirada no sistema de arquivo no formato de base64

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -20,6 +20,22 @@ export class PhotoService {
 
   constructor() {}
 
+  private async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+
+    const fileName = Date.now() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data,
+    });
+
+    return {
+      filepath: fileName,
+      webviewPath: photo.webPath,
+    } as UserPhoto;
+  }
+
   public async addNewPhotoToGallery() {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
@@ -27,9 +43,25 @@ export class PhotoService {
       quality: 100,
     });
 
-    this.photos.unshift({
-      filepath: '',
-      webviewPath: capturedPhoto.webPath!,
-    });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+
+    this.photos.unshift(savedImageFile);
   }
+
+  private async readAsBase64(photo: Photo) {
+    const response = await fetch(photo.webPath!);
+    const blob = await response.blob();
+    return (await this.convertBlobToBase64(blob)) as string;
+  }
+
+  private convertBlobToBase64 = (blob: Blob) =>
+    new Promise((resolve, reject) => {
+      const reader = new FileReader();
+      reader.onerror = reject;
+      reader.onload = () => {
+        resolve(reader.result);
+      };
+
+      reader.readAsDataURL(blob);
+    });
 }
